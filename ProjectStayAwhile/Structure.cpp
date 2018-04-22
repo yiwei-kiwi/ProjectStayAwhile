b143edbc82a5f0cb12059794cd8cdcab92cc2f8f
Yi Wei <31630096+yiwei-kiwi@users.noreply.github.com>
2018-04-22 13:51:49 -0700

    Add class Structure.

---
+++ ProjectStayAwhile/Structure.cpp
@@ -0,0 +1,15 @@
+#include "Structure.h"
+
+ARPG::Structure::Structure(Coord location, int length, int width) : Object(location, 's', length, width)
+{
+
+}
+
+ARPG::Structure::~Structure()
+{
+}
+
+bool ARPG::Structure::isObstruction() const
+{
+	return true;
+}
